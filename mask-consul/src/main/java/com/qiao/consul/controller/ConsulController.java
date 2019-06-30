f515eccfd8bb12828868efcb1d6e706ddb767e8c
huangqf <244154988@qq.com>
2019-06-30 11:39:17 +0800

    删除无效注释

--- mask-consul/src/main/java/com/qiao/consul/controller/ConsulController.java
+++ mask-consul/src/main/java/com/qiao/consul/controller/ConsulController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
  * @author huangqf
  * @date: 2019/6/29 15:05
  * @description:
- * @Copyright (C) 杭州同基汽车科技有限公司
  */
 @RestController
 @RequestMapping("/")
