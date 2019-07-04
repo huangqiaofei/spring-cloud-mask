8d7b4ad20b0d7b6ec2ef9d9cde6a5c436960eb71
huangqf <244154988@qq.com>
2019-07-04 14:14:00 +0800

    修改

--- mask-common/common-core/src/main/java/com/qiao/mask/common/config/CoreConfiguration.java
+++ mask-common/common-core/src/main/java/com/qiao/mask/common/config/CoreConfiguration.java
@@ -1,16 +1,4 @@
-/*
- * Copyright (c) 2018. paascloud.net All Rights Reserved.
- * 项目名称：paascloud快速搭建企业级分布式微服务平台
- * 类名称：CoreConfiguration.java
- * 创建人：刘兆明
- * 联系方式：paascloud.net@gmail.com
- * 开源地址: https://github.com/paascloud
- * 博客地址: http://blog.paascloud.net
- * 项目官网: http://paascloud.net
- */
-
 package com.qiao.mask.common.config;
-
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
